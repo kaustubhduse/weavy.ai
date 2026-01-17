0f5c6e65a908e814c077de9c10be46db4464ea74
kaustubhduse <202251045@iiitvadodara.ac.in>
2026-01-17 13:08:43 +0530

    Configure webpack to bundle FFmpeg binary for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@ffmpeg-installer/ffmpeg'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Copy ffmpeg binaries to output
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@ffmpeg-installer/ffmpeg': 'commonjs @ffmpeg-installer/ffmpeg',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
